e0be99e4a1b30735edee4ac4871354f14e90c480
claraharguindey <clarisaharguindey@gmail.com>
2023-08-16 20:00:01 +0200

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,41 @@
-"use client";
+'use client';
 import styles from './page.module.css';
-import { MouseEventHandler, useState } from 'react';
+import {
+  MouseEventHandler,
+  RefObject,
+  Suspense,
+  useEffect,
+  useState,
+} from 'react';
+import { useRef } from 'react';
 
 const Home = () => {
   const [clicksCount, setClickCount] = useState(0);
-  const [isInitialMsgHidden, setIsInitialMsgHidden] = useState(false);
-  const [posX, setPosX] = useState('');
-  const [posY, setPosY] = useState('');
+  const container = useRef<HTMLElement>(null);
+  const text0 = useRef<HTMLElement>(null);
+  const text1 = useRef<HTMLElement>(null);
+  const text2 = useRef<HTMLElement>(null);
+  const text3 = useRef<HTMLElement>(null);
+  const text4 = useRef<HTMLElement>(null);
+  const text5 = useRef<HTMLElement>(null);
+  const text6 = useRef<HTMLElement>(null);
+  const articles = [
+    text0,
+    text1,
+    text2,
+    text3,
+    text4,
+    text5,
+    text6,
+  ] as RefObject<HTMLElement>[];
 
-  const getMousePosition = (e: React.MouseEvent) => {
+  const getMousePosition = (
+    e: React.MouseEvent,
+    item: RefObject<HTMLElement>
+  ) => {
     var posx = 0;
     var posy = 0;
-    // if (!e) let e = window.event;
+
     if (e.pageX || e.pageY) {
       posx = e.pageX;
       posy = e.pageY;
@@ -19,128 +43,151 @@ const Home = () => {
       posx = e.clientX;
       posy = e.clientY;
     }
-    setPosX(`${posx}px`);
-    setPosY(`${posy}px`);
-    // document.querySelector(`.${item}${n}`).style.position = 'absolute';
-    // document.querySelector(`.${item}${n}`).style.left = `${posx}px`;
-    // document.querySelector(`.${item}${n}`).style.top = `${posy}px`;
-    // document.querySelector(`.${item}${n}`).style.display = 'block';
+
+    if (item.current) {
+      item.current.style.position = 'absolute';
+      item.current.style.left = `${posx}px`;
+      item.current.style.top = `${posy}px`;
+      item.current.style.display = 'block';
+    }
   };
 
+  const setGrid = () => {
+    if (container.current) {
+      container.current.className = `${styles.grid}`;
+    }
+  };
   const onClick = (event: React.MouseEvent) => {
     const currentClickCount = clicksCount + 1;
+    const TEXTS_LENGTH = 6;
+    if (clicksCount <= TEXTS_LENGTH) {
+      getMousePosition(event, articles[clicksCount]);
+    } else if (clicksCount > TEXTS_LENGTH) {
+      setGrid();
+    }
     setClickCount(currentClickCount);
   };
-  
+
   return (
     <main className={styles.main} onClick={onClick}>
-      <header className={styles.header}>
-        <div>{clicksCount}</div>
-        <nav>
-          <ul className={styles.navList}>
-            <li className={styles.navItem}>
-              <a href="/projects/project.html"></a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main className={styles.main}>
-        {clicksCount >= 1 ? (
-          <div className={styles.initMessage}>Haz CLICK.</div>
-        ) : null}
-        {clicksCount >= 1 ? (
-          <article className="article article0 hidden">
-            <p>
-              Clicar es un seminario sobre mediación cultural digital
-              comisariado por Desmusea que parte del programa caminar, clicar,
-              desplazar…, impulsado por el Departamento de Educación del Museo
-              Nacional Centro de Arte Reina Sofía. A través de los tres verbos:
-              caminar (coordinado por La Liminal), clicar y desplazar
-              (coordinado por Felipa Manuela) se pretende analizar los
-              diferentes acercamientos contemporáneos a la mediación cultural.
-              En un contexto en el que las instituciones culturales se
-              encuentran cada vez más atravesadas por la virtualidad, clicar
-              pretende definir un marco general en torno a la mediación cultural
-              digital – aquellas prácticas de mediación que hacen uso de la
-              tecnología formal o conceptualmente. clicar se articula a través
-              de encuentros de pensamiento y acciones específicas que amplíen
-              las posibilidades poéticas y políticas que se abren ante el
-              diálogo con la dimensión online de la institución. La primera
-              edición tuvo lugar entre mayo y septiembre de 2022 contando con
-              Elena Villaespesa, Gema Grueso, Mario Santamaría, Ontologías
-              Feministas y Agnès Pe.
-            </p>
-          </article>
-        ) : null}
-        {clicksCount >= 2 ? (
-          <article className="article article1 hidden">
-            <h2>Poéticas de lo hipervinculado</h2>
-            <p>
-              {' '}
-              Poéticas de lo hipervinculado pretende mostrar las posibilidades y
-              poéticas de lo que entendemos como mediación cultural digital:
-              prácticas de intermediación entre arte y públicos a través de una
-              tecnología que funciona como medio, y no como fin.
-            </p>
-          </article>
-        ) : null}
-        {clicksCount >= 3 ? (
-          <article className="article article2 hidden">
-            <h2>El usuario en el museo digital</h2>
-            <p>
-              En este encuentro con Elena Villaespesa significamos cómo, a la
-              hora de articular proyectos de mediación digital, resulta
-              importante personalizar a la persona que está al otro lado de la
-              pantalla para facilitar la interacción con nuestra propuesta.
-            </p>
-          </article>
-        ) : null}
-        {clicksCount >= 4 ? (
-          <article className="article article3 hidden">
-            <h2>Virtual infrastructour</h2>
-            <p>
-              Virtual Infrastructour, un itinerario digital realizado por Mario
-              Santamaría, nos revela cómo la mediación cultural digital nos
-              sirve como herramienta para transparentar el museo, percatarnos de
-              su funcionamiento interno, y cuestionarlo desde otros espacios de
-              pensamiento crítico.
-            </p>
-          </article>
-        ) : null}
-        {clicksCount >= 5 ? (
-          <article className="article article4 hidden">
-            <h2>Mediar el daño digital</h2>
-            <p>
-              Entendiendo el museo como un espacio en crisis continua, amenazado
-              por el daño, Ontologías Feministas nos propone abordar lo digital
-              como un espacio poético para plantear esta cuestión y encontrar
-              fórmulas de alivio y reparación.
-            </p>
-          </article>
-        ) : null}
-        {clicksCount >= 6 ? (
-          <article className="article article5 hidden">
-            <h2>Sostenibilidad y archivo</h2>
-            <p>
-              La conservadora Gema Grueso nos acompañó a reflexionar sobre la
-              conservación de proyectos digitales más allá de la temporalidad
-              inmediata, sobre la importancia del archivo en un contexto de
-              producción inmaterial, y las herramientas de las que podemos
-              disponer para desarrollar nuestros proyectos digitales.
-            </p>
-          </article>
-        ) : null}
-        {clicksCount >= 7 ? (
-          <article className="article article6 hidden">
-            <h2>Residuos sónicos</h2>
-            <p>
-              En la propuesta Residuos sónicos de Agnes Pe, atravesamos varios
-              espacios del museo despertando su sonoridad interna, y con ello,
-              dialogando con el edificio y su intrahistoria.
-            </p>
-          </article>
-        ) : null}
-      </main>
+      {clicksCount < 1 ? (
+        <div className={styles.initMessage}>CLICAR</div>
+      ) : null}
+      <section ref={container}>
+        <article
+          ref={text0}
+          className={`${styles.article} ${styles.article0} ${styles.hidden}`}
+        >
+          <p>
+            Clicar es un seminario de Mediación Cultural Digital comisariado
+            por{' '}
+            <a href="https://www.desmusea.com" target="_blanck">
+              Desmusea
+            </a>{' '}
+            que formó parte del programa <i>Caminar, clicar, desplazar </i>
+            impulsado por el Departamento de Educación del Museo Nacional Centro
+            de Arte Reina Sofía durante los meses de *** a *** de 2022.
+          </p>
+        </article>
+        <article
+          ref={text1}
+          className={`${styles.article} ${styles.article1} ${styles.hidden}`}
+        >
+          <h2 className={styles.title}>
+            1 <br />
+            <br />
+            Poéticas de lo hipervinculado
+          </h2>
+          <p>
+            {' '}
+            Poéticas de lo hipervinculado pretende mostrar las posibilidades y
+            poéticas de lo que entendemos como mediación cultural digital:
+            prácticas de intermediación entre arte y públicos a través de una
+            tecnología que funciona como medio, y no como fin.
+          </p>
+        </article>
+        <article
+          ref={text2}
+          className={`${styles.article} ${styles.article2} ${styles.hidden}`}
+        >
+          <h2 className={styles.title}>
+            2 <br />
+            <br />
+            El usuario en el museo digital
+          </h2>
+          <p>
+            En este encuentro con Elena Villaespesa significamos cómo, a la hora
+            de articular proyectos de mediación digital, resulta importante
+            personalizar a la persona que está al otro lado de la pantalla para
+            facilitar la interacción con nuestra propuesta.
+          </p>
+        </article>
+        <article
+          ref={text3}
+          className={`${styles.article} ${styles.article3} ${styles.hidden}`}
+        >
+          <h2 className={styles.title}>
+            3 <br />
+            <br />
+            Virtual infrastructour
+          </h2>
+          <p>
+            Virtual Infrastructour, un itinerario digital realizado por Mario
+            Santamaría, nos revela cómo la mediación cultural digital nos sirve
+            como herramienta para transparentar el museo, percatarnos de su
+            funcionamiento interno, y cuestionarlo desde otros espacios de
+            pensamiento crítico.
+          </p>
+        </article>
+        <article
+          ref={text4}
+          className={`${styles.article} ${styles.article4} ${styles.hidden}`}
+        >
+          <h2 className={styles.title}>
+            4 <br />
+            <br />
+            Mediar el daño digital
+          </h2>
+          <p>
+            Entendiendo el museo como un espacio en crisis continua, amenazado
+            por el daño, Ontologías Feministas nos propone abordar lo digital
+            como un espacio poético para plantear esta cuestión y encontrar
+            fórmulas de alivio y reparación.
+          </p>
+        </article>
+        <article
+          ref={text5}
+          className={`${styles.article} ${styles.article5} ${styles.hidden}`}
+        >
+          <h2 className={styles.title}>
+            5 <br />
+            <br />
+            Sostenibilidad y archivo
+          </h2>
+          <p>
+            La conservadora Gema Grueso nos acompañó a reflexionar sobre la
+            conservación de proyectos digitales más allá de la temporalidad
+            inmediata, sobre la importancia del archivo en un contexto de
+            producción inmaterial, y las herramientas de las que podemos
+            disponer para desarrollar nuestros proyectos digitales.
+          </p>
+        </article>
+        <article
+          ref={text6}
+          className={`${styles.article} ${styles.article6} ${styles.hidden}`}
+        >
+          <h2 className={styles.title}>
+            6 <br />
+            <br />
+            Residuos sónicos
+          </h2>
+          <p>
+            En la propuesta Residuos sónicos de Agnes Pe, atravesamos varios
+            espacios del museo despertando su sonoridad interna, y con ello,
+            dialogando con el edificio y su intrahistoria.
+          </p>
+        </article>
+      </section>
     </main>
   );
 };
---
+++ src/app/user/page.tsx
@@ -0,0 +1,55 @@
+export default function User() {
+  return (
+    <section>
+      <h1>el usuario en el museo virtual</h1>
+      <article>
+        <p>
+          <b>
+            En este encuentro con Elena Villaespesa significamos cómo, a la hora
+            de articular proyectos de mediación digital, resulta importante
+            personalizar a la persona que está al otro lado de la pantalla para
+            facilitar la interacción con nuestra propuesta.
+          </b>
+        </p>
+        <p>
+          La analista de datos Elena Villaespesa, que ha formado parte de los
+          equipos de estrategia digital de numerosos museos de Europa y Estados
+          Unidos, ha insistido desde hace años en la importancia creciente de la
+          parte digital de las instituciones culturales, hasta el punto que
+          museos como el Tate recibe más visitas a su web que la suma de sus
+          cuatro sedes físicas.
+        </p>
+        <p>
+          En este encuentro, Elena nos ofreció una panorámica de algunas
+          herramientas de determinación de las usuarias que entran en contacto
+          con el museo exclusivamente a través de la pantalla. Uno de ellos es
+          el <i>Visitor journey map</i> o Mapas de experiencia del usuario, que
+          delinean en qué términos tiene lugar este contacto entre institución y
+          visitante <i>online</i>. Esta herramienta permite conocer las
+          necesidades de la usuaria y ponerla en el centro a la hora de producir
+          este contenido, permitiendo que pueda completar sus objetivos. Por
+          ejemplo, Elena nos comentaba cómo la palabra <i>Obras</i> frente al
+          término <i>Colección</i>
+          en la página web de un museo permitía entender más claramente el
+          contenido de ese enlace, aumentando el número de <i>clics</i>.
+        </p>
+        <p>
+          Esta segmentación de públicos y puesta en relieve del perfil de la
+          usuaria (intereses, necesidades, emociones, oportunidades, etc.) no es
+          un proceso distinto de las preguntas que, como mediadoras, nos
+          planteamos a la hora de diseñar un taller presencial. ¿Cómo compartir
+          los conocimientos que queremos incluir? ¿Cómo plantear las preguntas?
+          ¿De qué manera distribuir las paradas del recorrido? ¿Cómo cambiamos
+          la dinámica si las participantes son pequeñas, adolescentes, adultas o
+          personas mayores?
+        </p>
+        <p>
+          Dirigirnos estas cuestiones a la hora de crear nuestro proyecto, y
+          plantear conversaciones con nuestro público para conocer sus
+          dificultades y preferencias, nos dirigen a mediaciones digitales más
+          accesibles, satisfactorias, y complejas.
+        </p>
+      </article>
+    </section>
+  );
+}
