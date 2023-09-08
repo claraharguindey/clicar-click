b8f4d72091161015c7c263f1f51a8743e5b394c9
claraharguindey <clarisaharguindey@gmail.com>
2023-09-08 20:44:07 +0200

    update text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,18 +14,41 @@ export default function About() {
       <h1 className="title">Clicar</h1>
       <article className="article">
         <p>
-          clicar fue un seminario sobre mediación cultural digital comisariado
-          por Desmusea que partió del programa{' '}
+          Clicar fue el seminario sobre mediación cultural digital comisariado
+          por Desmusea y en el que intervinieron diferentes artistas y
+          profesionales cuyo trabajo atraviesa los ámbitos del museo, la
+          mediación y la virtualidad. Clicar que formó parte del programa{' '}
           <Link href="https://www.museoreinasofia.es/actividades/caminar-clicar-desplazar">
-            caminar, clicar, desplazar…
+            Caminar, clicar, desplazar…
           </Link>
           , impulsado por el Departamento de Educación del Museo Nacional Centro
-          de Arte Reina Sofía. A través de los tres verbos: caminar (coordinado
+          de Arte Reina Sofía. A partir de los tres verbos: caminar (coordinado
           por <Link href="https://www.laliminal.com/">La Liminal</Link>),
           clicar, y desplazar (coordinado por{' '}
-          <Link href="https://felipamanuela.org/">Felipa Manuela</Link>) se
-          pretendió analizar diferentes acercamientos contemporáneos a la
-          mediación cultural.
+          <Link href="https://felipamanuela.org/">Felipa Manuela</Link>) la
+          propuesta pretendía analizar diferentes acercamientos contemporáneos a
+          la mediación cultural.
+          <br />
+          <br />
+          Los museos y las instituciones culturales son parte de un contexto
+          cada vez más atravesado por la virtualidad, en el que la producción y
+          el consumo de arte se han ido desplazando a los espacios web y en el
+          que las utopías del metaverso ya están presentes y son totalmente
+          inmediatas. Nuevas prácticas de mediación cultural, imbricadas en lo
+          rizomático, lo interconectado y lo digital, han ido tomando progresiva
+          fuerza en el territorio museístico, articulando nuevos vínculos entre
+          las propuestas artísticas y la ciudadanía.
+          <br /> <br />
+          Este nodo pretendió configurar un marco general en torno a la
+          mediación cultural digital, entendida como ejercicio de intermediación
+          artística que ubica el pensamiento tecnológico en el centro. Busca no
+          solo ofrecer un contexto teórico (que aterrice el concepto de
+          mediación cultural digital, la cuestión de los públicos en la
+          virtualidad y la conservación y archivo de estas prácticas), sino
+          también constituir un ejercicio propositivo que transforme estas ideas
+          en acciones específicas, ofreciendo una imagen prismática de
+          diferentes posibilidades poéticas y políticas que se abren ante el
+          diálogo con la dimensión online de la institució
         </p>
       </article>
     </section>
